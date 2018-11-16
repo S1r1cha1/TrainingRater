eb9f6f60a87d0ce887a7bdd71d10115e50a2622a
S1r1cha1 <sirichai@4miles.com>
2018-11-15 21:57:00 -0500

    add sequelize and mysql connection

--- config/config.js
+++ config/config.js
@@ -4,4 +4,4 @@ CONFIG.app = 'dev';
 CONFIG.db_dialect = 'mysql';
 CONFIG.db_name = 'TrainingRaterStarter';
 CONFIG.db_user = 'root';
-CONFIG.db_password = 'miles';
+CONFIG.db_password = 'F00tb@ll123456';
